24214083e7f62eae8db8ebbe28d93d344bf25313
Rob Klouda <rgklouda@comcast.net>
2017-03-03 16:11:20 +0000

    init

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var router = express();
 var server = http.createServer(router);
 var io = socketio.listen(server);
 
-router.use(express.static(path.resolve(__dirname, 'jetqm/dist')));
+router.use(express.static(path.resolve(__dirname, 'dist')));
 var messages = [];
 var sockets = [];
 
@@ -78,7 +78,7 @@ function broadcast(event, data) {
   });
 }
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+server.listen(process.env.PORT || 5000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
