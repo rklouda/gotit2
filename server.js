ce80cc6c28b76b414efa2de5bdf4023fb697200c
Rob Klouda <rgklouda@comcast.net>
2017-03-03 21:14:45 +0000

    init

--- server.js
+++ server.js
@@ -11,13 +11,14 @@ var socketio = require('socket.io');
 var express = require('express');
 
 var app = express();
-
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
-
-// make express look in the public directory for assets (css/js/img)
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.get('/', function(request, response) {
+  response.render('index')
+});
 
 //
 // ## SimpleServer `SimpleServer(obj)`
